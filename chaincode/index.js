ea5c5bca964a00eb7361aa95c80bd50cedb4067e
AfrinJubaida <37942277+AfrinJubaida@users.noreply.github.com>
2021-07-02 21:46:49 +0600

    Update index.js

--- chaincode/index.js
+++ chaincode/index.js
@@ -3,7 +3,7 @@
 const EHR = require('./lib/hoscert_contract');
 
 console.log(EHR)
-//NOTE: Estore was changed to Educert.
+//NOTE: Estore was changed to Medicert.
 //Todo: During chaincode invocation, each chaincode is given a name. Find out where that name originates from. 
 module.exports.EHR = EHR;
 module.exports.contracts = [ EHR ];
